1976cad76fb38ee9880c0bbc6790b6913490cb2d
Sabrina Hu <yourgitemail@example.com>
2023-06-14 16:19:21 -0500

    changes

---
+++ src/models/emotion_detection.py
@@ -0,0 +1,7 @@
+import tensorflow as tf
+import numpy as np
+import pandas as pd
+import cv2
+import keras
+from tensorflow.keras.applications.vgg16 import VGG16, preprocess_input
+from tensorflow.keras.applications import ResNet50
--- src/src/settings.py
+++ src/src/settings.py
@@ -15,6 +15,9 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# add model path
+MODELS = os.path.join(BASE_DIR, 'models')
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -37,6 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    # apps
+    'video_app'
 ]
 
 MIDDLEWARE = [
